0d3af4066112f63abc3d932a503593837402b609
Chernous Pavel <goltshteyngottorp@gmail.com>
2025-11-17 16:09:15 +0000

    Update project

--- megano/megano/urls.py
+++ megano/megano/urls.py
@@ -1,22 +1,18 @@
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.template.context_processors import static
 from django.urls import path, include
 
 from megano import settings
 
 urlpatterns = [
-
-    path('admin/', admin.site.urls),
-
-    path('', include("frontend.urls")),
+    path("admin/", admin.site.urls),
+    path("", include("frontend.urls")),
+    path("api/", include("accounts.urls")),
+    path("api/", include("shop.urls")),
+    path("api/", include("product.urls")),
+    path("api/", include("cart.urls")),
+    path("api/", include("order.urls")),
 ]
-
-# if settings.DEBUG:
-#     urlpatterns.extend(
-#         static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#     )
-#
-#     urlpatterns.extend(
-#         static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-#     )
-#
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += (path("__debug__/", include("debug_toolbar.urls")),)
---
+++ megano/product/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import (
+    ProductsDetailView,
+    ReviewProductView,
+)
+
+urlpatterns = [
+    path("product/<id>", ProductsDetailView.as_view(), name="product"),
+    path("product/<id>/reviews", ReviewProductView.as_view(), name="review"),
+]
