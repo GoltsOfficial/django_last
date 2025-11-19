77e2516bbbf8348bd9567500394509c903300911
Chernous Pavel <goltshteyngottorp@gmail.com>
2025-11-19 20:22:05 +0000

    Format base

--- megano/cart/serializers.py
+++
@@ -1,9 +0,0 @@
-from product.serializers import ProductSerializer
-from rest_framework import serializers
-
-
-class BasketSerializer(ProductSerializer):
-    count = serializers.SerializerMethodField()
-
-    def get_count(self, obj):
-        return self.context["count"][obj.id]
--- megano/cart/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-
-from .views import BasketView
-
-
-urlpatterns = [path("basket/", BasketView.as_view(), name="basket")]
