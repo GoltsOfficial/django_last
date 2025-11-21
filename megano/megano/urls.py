d424b45a0dd0cade1097638ac3ee1cfe1daaae0c
Chernous Pavel <goltshteyngottorp@gmail.com>
2025-11-21 09:48:25 +0000

    Right start

--- megano/megano/urls.py
+++ megano/megano/urls.py
@@ -1,17 +1,8 @@
-from django.urls import path, include
 from django.contrib import admin
-from rest_framework import routers
-
-from accounts.views import SignUpAPIView, SignInAPIView, SignOutAPIView
-
-
-router = routers.DefaultRouter()
-router.register(r'sign-in', SignInAPIView, basename='sign-in')
-router.register(r'sign-up', SignUpAPIView, basename='sign-up')
-router.register(r'sign-out', SignOutAPIView, basename='sign-out')
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
-    path('api/', include(router.urls)),
+    path('api/', include('api.urls')),  # Добавляем api маршруты
 ]
